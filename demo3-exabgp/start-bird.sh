ab67c4d449a4a04d8f6dff4c67272a0fd7b1ddf7
codybenz16 <codybenz16@gmail.com>
2025-07-22 15:39:44 -0500

    Update start-bird.sh

--- demo3-exabgp/start-bird.sh
+++ demo3-exabgp/start-bird.sh
@@ -2,16 +2,13 @@
 
 # note: no -it
 
-lan1="docker exec  clab-mod2-bgp-lan1"
-lan2="docker exec  clab-mod2-bgp-lan2"
-rtrA="docker exec  clab-mod2-bgp-rtrA"
-rtrB="docker exec  clab-mod2-bgp-rtrB"
-rtrC="docker exec  clab-mod2-bgp-rtrC"
-rtrD="docker exec  clab-mod2-bgp-rtrD"
+rtrA="docker exec -it clab-mod2-bgp-rtrA"
+rtrB="docker exec -it clab-mod2-bgp-rtrB"
+rtrC="docker exec -it clab-mod2-bgp-rtrC"
+rtrD="docker exec -it clab-mod2-bgp-rtrD"
 
-
-$rtrA bird -c /etc/bird-alt/rtrA-bird.conf
-$rtrB bird -c /etc/bird-alt/rtrB-bird.conf
-$rtrC bird -c /etc/bird-alt/rtrC-bird.conf
-$rtrD bird -c /etc/bird-alt/rtrD-bird.conf
+$rtrA bird -c /etc/bird/bird.conf &
+$rtrB bird -c /etc/bird/bird.conf &
+$rtrC bird -c /etc/bird/bird.conf &
+$rtrD bird -c /etc/bird/bird.conf &
 
